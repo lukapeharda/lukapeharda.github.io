f437c31f2e70ba1f9643e9e9fa3537d7380626cf
Luka Peharda <luka.peharda@gmail.com>
2019-11-14 20:54:55 +0100

    Update homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function IndexPage() {
         </div>
         <div className="w-full md:w-1/2 flex justify-center items-center p-6 md:p-8 py-6">
           <div className="text-white text-center md:text-left">
-            <p className="text-3xl pb-4">Hi!</p>
-            <p className="text-3xl">My name is Luka Peharda and I'am a <span className="border-b border-e6n-red">PHP</span> &amp; <span className="border-b border-e6n-blue">JS</span> developer with over 10 years of experience!</p>
+            <h2 className="text-4xl italic pb-4">Hi!</h2>
+            <p className="text-3xl">My name is Luka Peharda and I'am a <span className="text-e6n-red">PHP</span> &amp; <span className="text-e6n-blue">JS</span> developer with over 10 years of experience!</p>
           </div>
         </div>
       </div>
