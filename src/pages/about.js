1c59d0b96edbd0ffd821470dc01be5575828dd35
Luka Peharda <luka.peharda@gmail.com>
2019-11-15 11:09:51 +0100

    Remove unused images left from starter pack

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import helmet from "../images/dog-illustration.svg";
 
 function AboutPage() {
     return (
