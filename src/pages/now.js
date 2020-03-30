2956477252a428a0d7c85b989fc25c8070a93534
lukapeharda <luka.peharda@gmail.com>
2020-03-30 13:50:53 +0200

    Update now page with current stuff. Add IR#9 article and Radical Candor book recommendation

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,8 +6,8 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 function NowPage() {
-    const prev = new Date('2019-11-14');
-    const now = new Date('2020-01-14');
+    const prev = new Date('2020-01-14');
+    const now = new Date('2020-03-30');
     return (
         <Layout>
             <SEO
@@ -22,14 +22,14 @@ function NowPage() {
                 <h1 className="text-white light-mode:text-gray-800 italic text-4xl mt-8">What I'm doing right <span className="text-e6n-yellow">now</span></h1>
                 <p className="text-gray-500 light-mode:text-gray-800 text-sm pb-4">{ now.toDateString() }</p>
 
-                <p className="text-white light-mode:text-gray-800 text-xl">Working on funnel stats and funnel steps A/B variation testing for <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-green hover:bg-e6n-green" target="_blank">OptimizePress WordPress Page Builder</OutboundLink>.</p>
-                <p className="text-white light-mode:text-gray-800 text-xl">I'm adding new features to <OutboundLink href="https://photile.co/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">Photile.co</OutboundLink> and working on its landing page describing potential use cases.</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">Finishing up sales stats for <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">OptimizePress Checkouts</OutboundLink> WordPress plugin and starting work on recurring charges (subscriptions).</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">I've launched my first side project <OutboundLink href="https://photile.co/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">Photile.co</OutboundLink> on IndieHackers, HackerNews (Show HN) and ProductHunt. Now I'm just sitting back and waiting for users to start rolling in :-D</p>
 
                 <h2 className="text-white light-mode:text-gray-800 italic text-2xl mt-8">Previous update</h2>
                 <p className="text-gray-500 light-mode:text-gray-800 text-sm pb-4">{ prev.toDateString() }</p>
 
-                <p className="text-white light-mode:text-gray-800 text-xl">Currently I'm learning <OutboundLink href="https://www.gatsbyjs.org/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">Gatsby</OutboundLink> and <OutboundLink href="https://tailwindcss.com/" target="_blank" className="border-b border-e6n-blue hover:bg-e6n-blue">Tailwind CSS</OutboundLink> and working on my own portfolio page (which you are currently reading).</p>
-                <p className="text-white light-mode:text-gray-800 text-xl">Implementing <span className="text-e6n-blue">PayPal</span> subscriptions to <OutboundLink href="https://www.optimizepress.com/" target="_blank">OptimizePress</OutboundLink> checkout because PayPal is still a thing in a lot of countries and preferred method of payment for a lot of people. Now if only it wasn't such a mess to integrate...</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">Working on funnel stats and funnel steps A/B variation testing for <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-green hover:bg-e6n-green" target="_blank">OptimizePress WordPress Page Builder</OutboundLink>.</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">I'm adding new features to <OutboundLink href="https://photile.co/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">Photile.co</OutboundLink> and working on its landing page describing potential use cases.</p>
             </div>
         </Layout>
     );
