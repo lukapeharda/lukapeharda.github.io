c4f5cf785a9fcc106bb0828c1feabcfb47dec45a
lukapeharda <luka.peharda@gmail.com>
2021-02-23 10:40:33 +0100

    Remove unused component from "Writing year in review article" article

--- src/pages/article/writing-year-in-review-article.js
+++ src/pages/article/writing-year-in-review-article.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
 import Layout from "../../components/layout";
 import SEO from "../../components/seo";
