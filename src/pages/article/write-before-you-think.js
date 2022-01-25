a49de7462cd94c74dd7c829c708a9e4386433071
lukapeharda <luka.peharda@gmail.com>
2022-01-25 11:09:59 +0100

    Fix typo and quotes

--- src/pages/article/write-before-you-think.js
+++ src/pages/article/write-before-you-think.js
@@ -26,25 +26,25 @@ function WriteBeforeYouThink() {
                     <Paragraph>There are a lot of scenarios where writing makes me think clearly and what is more important, more efficiently. And it helps me to structure my thoughts.</Paragraph>
 
                     <h2 className="text-white light-mode:text-gray-800 text-2xl italic mt-8 mb-4">Solving coding problems</h2>
-                    <Paragraph>When I’m stuck on some coding problem or having a hard time debugging an issue I use writing to move everything from my head to make space for thinking and defining a context. Normally I use paper and pencil, but when they aren’t available I use Notion.</Paragraph>
+                    <Paragraph>When I'm stuck on some coding problem or having a hard time debugging an issue I use writing to move everything from my head to make space for thinking and defining a context. Normally I use paper and pencil, but when they aren't available I use Notion.</Paragraph>
                     <Divider></Divider>
                     <Paragraph>I use a process sprawling across a series of steps that guides me to my solution. The process and the steps were influenced by the <a href="https://fs.blog/feynman-technique/" target="_blank" className="border-b border-e6n-blue hover:bg-e6n-blue">Feynman Technique of Learning</a> but were heavily modified.</Paragraph>
                     <Divider></Divider>
 
                     <Paragraph><span className="text-e6n-red text-white mr-4">1.</span> First, I succinctly describe the problem, often I posit it as a question. This sets a context for me.</Paragraph>
-                    <Paragraph><span className="text-e6n-yellow text-white mr-4">2.</span> Then, similarly to Feynman Technique, I write down the stuff I know and stuff that I’m certain is true. In case of debugging an issue, I write down the consequences of the issue at hand, data (or database) state, and code (classes) involved.</Paragraph>
+                    <Paragraph><span className="text-e6n-yellow text-white mr-4">2.</span> Then, similarly to Feynman Technique, I write down the stuff I know and stuff that I'm certain is true. In case of debugging an issue, I write down the consequences of the issue at hand, data (or database) state, and code (classes) involved.</Paragraph>
                     <Paragraph><span className="text-e6n-green text-white mr-4">3.</span> I write down the unknowns and things I need to learn more about before making any judgment. And how to convert "unknowns" to "knowns".</Paragraph>
                     <Paragraph><span className="text-e6n-blue text-white mr-4">4.</span> Afterward, I move on to possible causes. After listing the most obvious culprits, I try to look outside the box and come up with barely plausible scenarios. For each of the items here I specify how to validate, confirm or discard the idea.</Paragraph>
-                    <Paragraph><span className="text-e6n-red text-white mr-4">5.</span> After clearing these steps pretty much everything is cleared up and there usually aren’t any big obstacles for developing a solution.</Paragraph>
+                    <Paragraph><span className="text-e6n-red text-white mr-4">5.</span> After clearing these steps pretty much everything is cleared up and there usually aren't any big obstacles for developing a solution.</Paragraph>
                     <Divider></Divider>
 
-                    <Paragraph>Naturally, sometimes these aren’t enough so additional steps are added to the process.</Paragraph>
-                    <Paragraph><span className="text-e6n-yellow text-white mr-4">6.</span> If the problem at hand is not urgent the I let it simmer for a while and let my unconscious thoughts work on it. I either take a walk, change the working environment (work from a coffee place, work from home), or even sleep on it.</Paragraph>
+                    <Paragraph>Naturally, sometimes these aren't enough so additional steps are added to the process.</Paragraph>
+                    <Paragraph><span className="text-e6n-yellow text-white mr-4">6.</span> If the problem at hand is not urgent then I let it simmer for a while and let my unconscious thoughts work on it. I either take a walk, change the working environment (work from a coffee place, work from home), or even sleep on it.</Paragraph>
                     <Paragraph><span className="text-e6n-green text-white mr-4">7.</span> The last step is basically waiting for a solution to strike me in my head :-)</Paragraph>
 
                     <h2 className="text-white light-mode:text-gray-800 text-2xl italic mt-8 mb-4">Making decisions</h2>
-                    <Paragraph>I use a similar process when I’m stuck at deciding which solution or idea to pursue.</Paragraph>
-                    <Paragraph>One difference is using second-order thinking, especially if I’m dealing with legacy code or working on code I didn’t write (or forgot about). I tend to really worry about <a href="https://fs.blog/chestertons-fence/" target="_blank" className="border-b border-e6n-blue hover:bg-e6n-blue">Chesterton’s Fence</a> kicking me in the but :-)</Paragraph>
+                    <Paragraph>I use a similar process when I'm stuck at deciding which solution or idea to pursue.</Paragraph>
+                    <Paragraph>One difference is using second-order thinking, especially if I'm dealing with legacy code or working on code I didn't write (or forgot about). I tend to really worry about <a href="https://fs.blog/chestertons-fence/" target="_blank" className="border-b border-e6n-blue hover:bg-e6n-blue">Chesterton's Fence</a> kicking me in the but :-)</Paragraph>
                     <Paragraph>The additional benefit of writing down your decision process is that you can easily convert conclusions in your <a href="https://fs.blog/decision-journal/" target="_blank" className="border-b border-e6n-blue hover:bg-e6n-blue">decision diary</a>. You are keeping that, right? How else would you preserve knowledge in your org?</Paragraph>
                     <Paragraph></Paragraph>
 
