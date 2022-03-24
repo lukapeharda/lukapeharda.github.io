5a9e18a09893dc3f0e20b70325f41a912c68ab4f
lukapeharda <luka.peharda@gmail.com>
2022-03-24 08:30:20 +0100

    Update now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 function NowPage() {
-    const now = new Date('2021-11-26');
+    const now = new Date('2022-03-24');
     return (
         <Layout>
             <SEO
@@ -21,9 +21,8 @@ function NowPage() {
                 <h1 className="text-white light-mode:text-gray-800 italic text-4xl mt-8">What I'm doing right <span className="text-e6n-yellow">now</span></h1>
                 <p className="text-gray-500 light-mode:text-gray-800 text-sm pb-4">{ now.toDateString() }</p>
 
-                <p className="text-white light-mode:text-gray-800 text-xl">After a couple months of hiatus, I'm back working on <OutboundLink href="https://kourses.com/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">Kourses</OutboundLink>, <span className="text-gray-500">our unreleased membership SaaS product.</span> Currently, I'm working on API and integrating it with our own <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">OptimizePress</OutboundLink> members' hub to give customers extra content and value. This means that we are starting to eat our own dog food and use our product :-)</p>
-
-                <p className="text-white light-mode:text-gray-800 text-xl">As winter has arrived improvment of my MTB skills have moved from real life to PS4 and Riders Republic :-D</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">Currently, I'm implementing new payment flows and updating old ones (with the knowledge accrued from <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">OptimizePress</OutboundLink>) to <OutboundLink href="https://kourses.com/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">Kourses</OutboundLink>, our SaaS product for creating courses (which is now in closed beta). As 3D Secure 2 and PSD2 are ramping up it was about time to handle required authorization flows.</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">I'm working on adding extra membership features so you can build whole membership site in one place.</p>
             </div>
         </Layout>
     );
