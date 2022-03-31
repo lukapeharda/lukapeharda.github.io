6ec7fccc548891204031472c178fafb0546683c2
lukapeharda <luka.peharda@gmail.com>
2022-03-31 10:29:17 +0200

    Get rid of widow in last article headline (on articles list page)
    This is called a widow, sometimes an orphan or "runt"

--- src/components/articles.js
+++ src/components/articles.js
@@ -70,7 +70,7 @@ function Articles() {
                 { articles.map((article, index) => (
                     <div className="w-full md:w-1/2 lg:w-1/3 py-2 px-4" key={ article.node.id }>
                         <h3>
-                            <ArticleLink className={ `text-white light-mode:text-gray-800 p-1 text-lg border-b border-e6n-${colors[index % 4]} hover:bg-e6n-${colors[index % 4]}` } href={ article.node.url } title={ article.node.title } target="_blank">{ article.node.title }</ArticleLink>
+                            <ArticleLink className={ `text-white light-mode:text-gray-800 p-1 text-lg border-b border-e6n-${colors[index % 4]} hover:bg-e6n-${colors[index % 4]}` } href={ article.node.url } title={ article.node.title.replace('&nbsp;', '') } target="_blank" dangerouslySetInnerHTML={{ __html: article.node.title }}></ArticleLink>
                         </h3>
                         <div className="text-gray-500 light-mode:text-gray-800 text-xs mt-2">{ new Date(article.node.date).toDateString() }</div>
                         <p className="text-gray-100 light-mode:text-gray-600 mt-2">{ article.node.excerpt }</p>
