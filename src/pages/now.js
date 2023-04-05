d53d0c28efd5f092a39c51512f49a26775b71807
lukapeharda <luka.peharda@gmail.com>
2023-04-05 15:55:00 +0200

    Update now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 function NowPage() {
-    const now = new Date('2022-03-24');
+    const now = new Date('2023-04-05');
     return (
         <Layout>
             <SEO
@@ -21,8 +21,9 @@ function NowPage() {
                 <h1 className="text-white light-mode:text-gray-800 italic text-4xl mt-8">What I'm doing right <span className="text-e6n-yellow">now</span></h1>
                 <p className="text-gray-500 light-mode:text-gray-800 text-sm pb-4">{ now.toDateString() }</p>
 
-                <p className="text-white light-mode:text-gray-800 text-xl">Currently, I'm implementing new payment flows and updating old ones (with the knowledge accrued from <OutboundLink href="https://optimizepress.com/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">OptimizePress</OutboundLink>) to <OutboundLink href="https://kourses.com/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">Kourses</OutboundLink>, our SaaS product for creating courses (which is now in closed beta). As 3D Secure 2 and PSD2 are ramping up it was about time to handle required authorization flows.</p>
-                <p className="text-white light-mode:text-gray-800 text-xl">I'm working on adding extra membership features so you can build whole membership site in one place.</p>
+                <p className="text-white light-mode:text-gray-800 text-xl">Implementing search experience for <OutboundLink href="https://kourses.com/" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">Kourses</OutboundLink> using <OutboundLink href="https://www.meilisearch.com/" className="border-b border-e6n-red hover:bg-e6n-red" target="_blank">Meilisearch</OutboundLink>. Currently we are dogfooding<sup>1</sup> Kourses for <OutboundLink href="https://university.optimizepress.com/login" className="border-b border-e6n-blue hover:bg-e6n-blue" target="_blank">OptimizePress University</OutboundLink> but closed beta should be out soon as search is one of the last feature to be added for beta 1.</p>
+
+                <p className="text-gray-500 light-mode:text-gray-800 text-xl"><sup>1</sup> Dogfooding is a term used when you use a product you've built your self as a customer.</p>
             </div>
         </Layout>
     );
